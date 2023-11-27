a65217bbaf7c29b3d17bed218b4fbd74cbf8620e
soheilMansoori <ahadmansoori369@gamil.com>
2023-11-27 22:42:27 +0330

    fix router and usersPage

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,13 +1,14 @@
 import Articles from '../Pages/Articles';
 import Courses from '../Pages/Courses'
 import Infos from '../Pages/Infos'
-import Users from '../Pages/Users'
+import Users from '../Pages/Users/Users'
 
 const router = [
+    { path: '/', element: <Users /> },
+    { path: '/users', element: <Users /> },
     { path: '/articles', element: <Articles /> },
     { path: '/courses', element: <Courses /> },
     { path: '/infos', element: <Infos /> },
-    { path: '/users', element: <Users /> },
 ]
 
 export default router
