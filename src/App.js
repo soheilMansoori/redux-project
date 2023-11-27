a3cf3d34a01a2bbc0c6b93373b8025699b16ca94
soheilMansoori <ahadmansoori369@gamil.com>
2023-11-27 20:01:06 +0330

    add Sidebar.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRoutes } from 'react-router-dom'
 import router from './Router/router';
 import './App.css'
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   const routes = useRoutes(router)
@@ -9,7 +10,12 @@ function App() {
     <>
       <div class="container px-0">
         <Header />
-        {routes}
+        <main class="main">
+          <div class="row justify-content-between mx-0">
+            <Sidebar />
+            {routes}
+          </div>
+        </main>
       </div>
     </>
   );
