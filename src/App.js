19a46d53b3a84c915906494aada3a7bce9f504a4
soheilMansoori <ahadmansoori369@gamil.com>
2023-11-27 19:28:38 +0330

    add Header component  to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useRoutes } from 'react-router-dom'
 import router from './Router/router';
 import './App.css'
+import Header from './Components/Header/Header';
 
 function App() {
   const routes = useRoutes(router)
   return (
     <>
-      <h1>hi this is react-redux-project</h1>
-      {routes}
+      <div class="container px-0">
+        <Header />
+        {routes}
+      </div>
     </>
   );
 }
