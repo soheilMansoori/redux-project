e2c10ec2f1febb107676d90a715f5db2538ec852
soheilMansoori <ahadmansoori369@gamil.com>
2023-11-28 18:46:13 +0330

    move in new folder and delete files

--- src/Pages/Articles.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Articles() {
-  return (
-    <div>
-      Articles page
-    </div>
-  )
-}
--- src/Pages/Infos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Infos() {
-  return (
-    <div>
-      Infos page
-    </div>
-  )
-}
