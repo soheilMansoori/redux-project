f91bd6c1d79cf54676d8889130c96b0d0ae2d1f0
soheilMansoori <ahadmansoori369@gamil.com>
2023-11-29 15:59:33 +0330

    use axios in project

---
+++ src/Axios/config.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const apiRequests = axios.create({
+    baseURL: 'https://redux-cms.iran.liara.run/api',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+apiRequests.interceptors.request.use(
+    (config) => {
+        // console.log('config => ', config);
+        return config
+    },
+
+    (error) => {
+        console.log("error => ", error);
+        return Promise.reject(error);
+    }
+);
+
+
+apiRequests.interceptors.response.use(
+    (response) => {
+        // console.log('response =>', response);
+        return response
+    },
+    (error) => {
+        console.log('error =>', error);
+        return Promise.reject(error)
+    }
+);
+
+
+export default apiRequests
